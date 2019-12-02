439443ab145e0daeeb07190006da614a80799a40
Hong Ooi <hongooi@microsoft.com>
2019-12-02 18:17:01 +1100

    check preds are good

--- 04_test_service.R
+++ 04_test_service.R
@@ -5,13 +5,17 @@ ingr_uri_secure <- sprintf("https://%s.%s.cloudapp.azure.com/score", aks_name, r
 # check the cert is from Lets Encrypt
 httr::BROWSE(ingr_uri_secure)
 
-httr::content(
-    httr::POST(
-        ingr_uri_secure,
-        httr::authenticate(username, password),
-        body=list(df=MASS::Boston[1:10, ]),
-        encode="json"
-    ),
-    simplifyVector=TRUE
+# get some predicted values
+res <- httr::POST(
+    ingr_uri_secure,
+    httr::authenticate(username, password),
+    body=list(df=MASS::Boston[1:10, ]),
+    encode="json"
 )
+httr::stop_for_status(res)
+
+pred <- httr::content(res, simplifyVector=TRUE)
+if(length(pred) != 10 || !is.numeric(pred))
+    stop("Bad predictions")
+
 
