cee3c70bf6702046f5de41b28660bae30a58e932
Hong Ooi <hongooi@microsoft.com>
2020-04-30 23:19:23 +1000

    Fixes for April AKS/AzureContainers updates (#11)
    * aks fixes
    
    * don't need Graph login anymore

--- 01_create_resources.R
+++ 01_create_resources.R
@@ -1,4 +1,3 @@
-library(AzureGraph)
 library(AzureRMR)
 library(AzureContainers)
 
@@ -6,14 +5,10 @@ library(AzureContainers)
 
 source("resource_specs.R")
 
-# create ARM and Graph logins
+# create ARM login
 az <- try(get_azure_login(tenant), silent=TRUE)
 if(inherits(az, "try-error"))
     az <- create_azure_login(tenant, auth_type="device_code")
-
-gr <- try(get_graph_login(tenant), silent=TRUE)
-if(inherits(gr, "try-error"))
-    gr <- create_graph_login(tenant, auth_type="device_code")
 
 sub <- az$get_subscription(sub_id)
 
@@ -24,18 +19,16 @@ else sub$create_resource_group(rg_name, location=rg_loc))
 # create a container registry
 deployresgrp$create_acr(acr_name)
 
-# create a Kubernetes cluster
-# this will take several minutes (usually 10-20)
+# create a Kubernetes cluster -- this will take a few minutes
 deployclus_svc <- deployresgrp$create_aks(aks_name,
     enable_rbac=TRUE,
     agent_pools=aks_pools("agentpool", num_nodes, node_size))
 
 
 # give the cluster access to the registry
-aks_app_id <- deployclus_svc$properties$servicePrincipalProfile$clientId
 deployreg_svc <- deployresgrp$get_acr(acr_name)
 deployreg_svc$add_role_assignment(
-    principal=gr$get_app(aks_app_id),
+    principal=deployclus_svc,
     role="Acrpull"
 )
 
